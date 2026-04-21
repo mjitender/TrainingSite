93bebec5f4f44c5d6ed12cdf0e876dad73ddee17
Rishi Rusia <rishirusia1@gmail.com>
2026-04-21 11:38:41 +0530

    Promo component updated

--- examples/kit-nextjs-skate-park/.sitecore/component-map.ts
+++ examples/kit-nextjs-skate-park/.sitecore/component-map.ts
@@ -9,7 +9,7 @@ import * as Title from 'src/components/title/Title';
 import * as StructuredData from 'src/components/structured-data/StructuredData';
 import * as RowSplitter from 'src/components/row-splitter/RowSplitter';
 import * as RichText from 'src/components/rich-text/RichText';
-//import * as Promo from 'src/components/promo/Promo';
+import * as Promo from 'src/components/promo/Promo';
 import * as CustomBanner from 'src/components/promo/CustomBanner';
 import * as PartialDesignDynamicPlaceholder from 'src/components/partial-design-dynamic-placeholder/PartialDesignDynamicPlaceholder';
 import * as PageContent from 'src/components/page-content/PageContent';
@@ -29,7 +29,7 @@ export const componentMap = new Map<string, NextjsContentSdkComponent>([
   ['StructuredData', { ...StructuredData }],
   ['RowSplitter', { ...RowSplitter }],
   ['RichText', { ...RichText }],
-  // ['Promo', { ...Promo }],
+  ['Promo', { ...Promo }],
   ['CustomBanner', { ...CustomBanner }],
   ['PartialDesignDynamicPlaceholder', { ...PartialDesignDynamicPlaceholder }],
   ['PageContent', { ...PageContent }],
